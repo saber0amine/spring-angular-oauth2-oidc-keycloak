6401122ca488cc653cc2ba9e3ed8386f80b1f4a1
saber0amine <saberamine000@gmail.com>
2024-08-07 19:41:00 +0100

    add project

---
+++ angular-front-app/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { ProductsComponent } from './products/products.component';
+import {HttpClientModule} from "@angular/common/http";
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://185.97.144.204:8095',
+        realm: 'amasoft-realm',
+        clientId: 'amasoft-angular-app'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        checkLoginIframe : true,
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    }).catch(error =>  {
+      console.error('Keycloak initialization failed', error);
+    })  ;
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomersComponent,
+    ProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    KeycloakAngularModule
+  ],
+  providers: [
+    {provide : APP_INITIALIZER, deps : [KeycloakService],useFactory : initializeKeycloak, multi : true}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
